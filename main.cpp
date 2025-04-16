aba02e61dc2385d5aaab12f85f99317d5959bdcf
Emils Berzs <emilsberzs@hotmail.com>
2025-04-16 12:30:52 +0100

    Added menu outline

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	//1- Print help
+	std::cout << "1: Print Help" << std::endl;
+	//2- Print exchange stats
+	std::cout << "2: Print exchange stats" << std::endl;
+	//3- Make an offer
+	std::cout << "3: Make an offer" << std::endl;
+	//4- Make a bid
+	std::cout << "4: Make a bid" << std::endl;
+	//5- View wallet
+	std::cout << "5: View wallet" << std::endl;
+	//6- Next timestep/Continue
+	std::cout << "6: Next Timestep/Continue" << std::endl;
+	return 0;
+}
