919281049abf35497497da06d8b8cbbffdadaf06
Emils Berzs <emilsberzs@hotmail.com>
2025-04-16 13:49:18 +0100

    Refactoring code into functions

--- main.cpp
+++ main.cpp
@@ -1,64 +1,114 @@
 #include <iostream>
 
-int main()
+void printMenu()
 {
-	while (true) {
-		//1- Print help
-		std::cout << "1: Print Help" << std::endl;
-		//2- Print exchange stats
-		std::cout << "2: Print exchange stats" << std::endl;
-		//3- Make an offer
-		std::cout << "3: Make an offer" << std::endl;
-		//4- Make a bid
-		std::cout << "4: Make a bid" << std::endl;
-		//5- View wallet
-		std::cout << "5: View wallet" << std::endl;
-		//6- Next timestep/Continue
-		std::cout << "6: Next Timestep/Continue" << std::endl;
+	//1- Print help
+	std::cout << "1: Print Help" << std::endl;
+	//2- Print exchange stats
+	std::cout << "2: Print exchange stats" << std::endl;
+	//3- Make an offer
+	std::cout << "3: Make an offer" << std::endl;
+	//4- Make a bid
+	std::cout << "4: Make a bid" << std::endl;
+	//5- View wallet
+	std::cout << "5: View wallet" << std::endl;
+	//6- Next timestep/Continue
+	std::cout << "6: Next Timestep/Continue" << std::endl;
 
-		std::cout << "==========================" << std::endl;
-		std::cout << "Type in 1-6 to choose an option" << std::endl;
+	std::cout << "==========================" << std::endl;
+	std::cout << "Type in 1-6 to choose an option" << std::endl;
+
+}
+
+int getUserOption() 
+{
+	int userOption;
+	std::cin >> userOption;
+	std::cout << "You chose: " << userOption << std::endl;
+	return userOption;
+
+}
+
+void processUserOption(int userOption)
+{
+	if (userOption == 0 || userOption > 6) //bad input
+
+	{
+
+		std::cout << "Invalid choice. Choose 1-6" << std::endl;
+
+	}
 
 
-		int userOption;
-		std::cin >> userOption;
-		std::cout << "You chose: " << userOption << std::endl;
 
-		if (userOption == 0 || userOption>6) //bad input
-		{
-			std::cout << "Invalid choice. Choose 1-6" << std::endl;
-		}
+	if (userOption == 1)
 
-		if (userOption == 1)
-		{
-			std::cout << "Your aim is to make money. Analize the market and make bids and offers" << std::endl;
-		}
+	{
 
-		if (userOption == 2)
-		{
-			std::cout << "Market looks good" << std::endl;
-		}
+		std::cout << "Your aim is to make money. Analize the market and make bids and offers" << std::endl;
 
-		if (userOption == 3)
-		{
-			std::cout << "Make an offer." << std::endl;
-		}
+	}
 
-		if (userOption == 4)
-		{
-			std::cout << "Make a bid - enter the amount" << std::endl;
-		}
 
-		if (userOption == 5)
-		{
-			std::cout << "Your wallet is empty" << std::endl;
-		}
 
-		if (userOption == 6)
-		{
-			std::cout << "Next timestamp/continue" << std::endl;
-		}
+	if (userOption == 2)
 
+	{
+
+		std::cout << "Market looks good" << std::endl;
+
+	}
+
+
+
+	if (userOption == 3)
+
+	{
+
+		std::cout << "Make an offer." << std::endl;
+
+	}
+
+
+
+	if (userOption == 4)
+
+	{
+
+		std::cout << "Make a bid - enter the amount" << std::endl;
+
+	}
+
+
+
+	if (userOption == 5)
+
+	{
+
+		std::cout << "Your wallet is empty" << std::endl;
+
+	}
+
+
+
+	if (userOption == 6)
+
+	{
+
+		std::cout << "Next timestamp/continue" << std::endl;
+
+	}
+}
+
+int main()
+{	
+	while (true) 
+	{
+		
+		printMenu();		
+		int userOption = getUserOption();
+		processUserOption(userOption);
+	
 	}
 	return 0;
 }
