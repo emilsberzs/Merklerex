c8f259f8ae0d8158ecdc90cdb8eaff290045fdb1
Emils Berzs <emilsberzs@hotmail.com>
2025-04-18 09:43:08 +0100

    OrderBookEntry header and seperate cpp files

---
+++ OrderBookEntry.h
@@ -0,0 +1,23 @@
+#include <string>
+
+enum class OrderBookType { bid, ask };
+
+class OrderBookEntry
+{
+public:
+
+
+	OrderBookEntry(
+		double price,
+		double amount,
+		std::string timestamp,
+		std::string product,
+		OrderBookType orderType
+	);
+		
+	double price;
+	double amount;
+	std::string timestamp;
+	std::string product;
+	OrderBookType orderType;
+};
